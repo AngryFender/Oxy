5dd9cfbfb35319f461c7d198acd5b4c0f74cffd0
AngryFender <Teabiscuitperson@gmail.com>
2024-09-27 01:14:40 +0100

    Added "run" option in the cli argument to spawn process and execute sh command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 use std::env;
+use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
+    if env::consts::OS != "linux" {
+        return;
+    }
+    println!("Starting, Oxy!");
+
     let args: Vec<String> = env::args().collect();
-    println!("Hello, Oxy!");
-    dbg!(args);
+    if args.len() <3{
+        return;
+    }
+
+    if args[1] == "run" {
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg(args[2].to_string())
+            .output()
+            .expect("failed to execute process");
+
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
+    }
 }
