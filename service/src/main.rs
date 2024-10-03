a7dbbedecd14055e98b380be82f8c0ad9393ef32
AngryFender <Teabiscuitperson@gmail.com>
2024-10-03 20:41:24 +0100

    Commands are executed in the backgound service

--- service/src/main.rs
+++ service/src/main.rs
@@ -10,13 +10,20 @@ fn main() {
     let mut pipe = Pipe::with_name("oxy_pipe").unwrap();
     println!("Pipe path:{}", pipe.path().display());
 
+
     for line in std::io::BufReader::new(pipe).lines(){
+        thread::sleep_ms(1000);
         let output = Command::new("sh")
             .arg("-c")
             .arg(line.unwrap())
             .output()
             .expect("failed to execute process");
 
-        print!("{}",String::from_utf8_lossy(&output.stdout));
+        thread::sleep_ms(1000);
+        let outputMessage =  String::from_utf8_lossy(&output.stdout);
+        print!("{}",outputMessage);
+        let mut outputPipe = Pipe::with_name("oxy_pipe_output").unwrap();
+        writeln!(&mut outputPipe,"this is a test").unwrap();
+        writeln!(&mut outputPipe,"{}", outputMessage).unwrap();
     }
 }
