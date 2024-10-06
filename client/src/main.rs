548eeebd6f2209bb22bd210dc2f6c4b645691314
AngryFender <Teabiscuitperson@gmail.com>
2024-10-06 22:56:41 +0100

    Updated the client so it sends the bash command and pid separated by ";;" delimiter. Client also starts listening on the pid specific pipe to receive the output from the service

--- client/src/main.rs
+++ client/src/main.rs
@@ -29,17 +29,19 @@ fn main() {
 
     if args[1] == "run" {
         let mut pipe = Pipe::with_name("oxy_pipe").unwrap();
-        writeln!(&mut pipe,"{} {}", args[2].to_string(), currentPid);
+        println!("Requested command: {}",args[2]);
+        writeln!(&mut pipe,"{}{}{}", args[2].to_string(),";;",currentPid);
     }
 
-//    let outputPipeName: String = "oxy_pip_output_" + currentPid;
-    let mut outputPipe = Pipe::with_name("oxy_output_pipe").unwrap();
+    let outputPipeName: String = "oxy_pip_output_".to_string() + &currentPid;
+    let mut outputPipe = Pipe::with_name(&outputPipeName).unwrap();
     for line in std::io::BufReader::new(outputPipe).lines(){
         let lineOutput: String = line.unwrap();
-        println!("{}",lineOutput);
         match lineOutput == "Oxy-over" {
             true => { break; }
-            false => {}
+            false => {
+                println!(" ↲ {}", lineOutput);
+            }
         }
     }
 }
