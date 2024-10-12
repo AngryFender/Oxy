328a4f8205eb872ffd7f6098d75f658847f650d9
R <Teabiscuitperson@gmail.com>
2024-10-12 23:42:19 +0100

    Added TempPipe to client as well

---
+++ client/src/tempipe.rs
@@ -0,0 +1,32 @@
+use std::fs;
+use ipipe::Pipe;
+use std::path::Path;
+use std::ffi::OsStr;
+
+pub(crate) struct TempPipe {
+    pipe: Pipe,
+}
+impl TempPipe {
+    pub(crate) fn new(name: &str) -> Self {
+        let pipe = Pipe::with_name(name).expect("Failed to create oxy_pipe");
+
+        Self{
+            pipe
+        }
+    }
+    pub(crate) fn get_pipe(&mut self) -> &mut Pipe {
+        &mut self.pipe
+    }
+    pub(crate) fn get_path(&self) -> &std::path::Path {
+        &self.pipe.path()
+    }
+}
+
+impl Drop for TempPipe {
+    fn drop(&mut self) {
+        match fs::remove_file(&self.pipe.path()) {
+            Ok(_) => println!("Successfully removed the named pipe: {}", &self.pipe.path().display()),
+            Err(e) => eprintln!("Failed to remove the named pipe: {}", &self.pipe.path().display()),
+        }
+    }
+}
